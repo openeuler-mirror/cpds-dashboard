f88cf36c96e53756d747882ccafdcb37d48c889c
SaarHV <dev16102@linx-info.com>
2022-11-18 13:47:47 +0800

    Import Element-Plus framework fully

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import './style.css'
 import App from './App.vue'
 import router from "./router";
 import pinia from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
-app.use(router).use(pinia).mount("#app");
+app.use(router).use(pinia).use(ElementPlus).mount("#app");
