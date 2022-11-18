a5a72ffe9d9aa43ab6fc259feebeb96816f494c2
SaarHV <dev16102@linx-info.com>
2022-11-18 15:34:59 +0800

    Import element-plus-icon as global component

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ import router from "./router";
 import pinia from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
+
+//import element-plus-icon
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 
 app.use(router).use(pinia).use(ElementPlus).mount("#app");
