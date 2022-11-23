d6d0b308f3020cbd3a15aae3abea946afca32ebd
SaarHV <dev16102@linx-info.com>
2022-11-23 10:27:12 +0800

    Alias 'src' as '/@', and use absolute paths to manage the project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 
-import './style.css'
-import App from './App.vue'
-import router from './router'
-import pinia from './store'
+import '/@/style.css'
+import App from '/@/App.vue'
+import router from '/@/router'
+import pinia from '/@/store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
--- src/router/index.ts
+++ src/router/index.ts
@@ -8,7 +8,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/home',
     name: 'home',
-    component: () => import('../views/home/index.vue')
+    component: () => import('/@/views/home/index.vue')
   }
 ]
 const router = createRouter({
