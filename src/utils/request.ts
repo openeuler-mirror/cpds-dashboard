57d78520bf9cb2c64e040e2b23920a5524cc8975
SaarHV <dev16102@linx-info.com>
2022-11-23 09:31:24 +0800

    Add request and response interceptor to check token

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import { ElMessage, ElMessageBox } from 'element-plus'
+import { Session } from './storage'
 
 // Configure a new axios instance
 const service = axios.create({
@@ -7,5 +9,41 @@ const service = axios.create({
   headers: { 'Content-Type': 'application/json' }
 })
 
+// add request interceptor
+service.interceptors.request.use(
+  (config) => {
+    if (Session.get('token')) {
+      ;(<any>config.headers).common['Authorization'] = `${Session.get('token')}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+// add response interceptor
+service.interceptors.response.use(
+  (response) => {
+    const res = response.data
+    if (res.code && res.code !== 0) {
+      // not login
+      if (res.code === 401) {
+        Session.clear()
+        window.location.href = '/'
+        ElMessageBox.alert('login please', 'notification', {})
+          .then(() => {})
+          .catch(() => {})
+      }
+      return Promise.reject(service.interceptors.response)
+    } else {
+      return response.data
+    }
+  },
+  (error) => {
+    ElMessage.error(error)
+    return Promise.reject(error)
+  }
+)
 // export axios
 export default service
