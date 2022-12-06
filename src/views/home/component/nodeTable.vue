4f74d293ac082009e91cf1fafddd8c4a192777a2
SaarHV <dev16102@linx-info.com>
2022-12-06 15:53:25 +0800

    Add node table in home page

---
+++ src/views/home/component/nodeTable.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-table border class="node-table">
+    <el-table-column prop="name" :label="$t('home.nodeTable.Name')" width="180" />
+    <el-table-column prop="ipaddress" :label="$t('home.nodeTable.IpAddress')" />
+    <el-table-column prop="status" :label="$t('home.nodeTable.Status')" />
+    <el-table-column prop="malfunction" :label="$t('home.nodeTable.Malfunction')" />
+    <el-table-column prop="subhelth" :label="$t('home.nodeTable.SubHealth')" />
+  </el-table>
+</template>
+
+<script lang="ts" setup></script>
+<style>
+.node-table {
+  width: 80%;
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,10 @@
 <template>
   <nodeChart />
+  <nodeTable />
 </template>
 <script setup lang="ts">
 import nodeChart from './component/nodeChart.vue'
+import nodeTable from './component/nodeTable.vue'
 </script>
 
 <style scoped></style>
