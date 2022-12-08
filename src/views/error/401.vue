3a13dcebc01e938c8fbdbee86eb153c3aa1b64da
SaarHV <dev16102@linx-info.com>
2022-12-08 18:25:14 +0800

    Add 401 page

---
+++ src/views/error/401.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>401</div>
+  <div>您暂无权限访问</div>
+  <div>
+    <el-button type="primary" round @click="onSetAuth">重新授权</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+import { Session } from '/@/utils/storage'
+
+export default defineComponent({
+  name: '401',
+  setup() {
+    const router = useRouter()
+    const onSetAuth = () => {
+      Session.clear()
+      router.push('/login')
+    }
+    return {
+      onSetAuth
+    }
+  }
+})
+</script>
