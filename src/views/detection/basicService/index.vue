b3bc5c5e8c4230dffe8100696b4bd4cbbe7c7c1f
SaarHV <dev16102@linx-info.com>
2022-12-13 11:33:55 +0800

    Add element-plus' Table component,start secondary encapsulation

--- src/views/detection/basicService/index.vue
+++ src/views/detection/basicService/index.vue
@@ -1,3 +1,8 @@
-<template>basicService</template>
-<script lang="ts" setup></script>
+<template>
+  basicService
+  <Table />
+</template>
+<script lang="ts" setup>
+import Table from '/@/components/common/table/index.vue'
+</script>
 <style scoped lang="scss"></style>
