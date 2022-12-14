d2039ca94edd41c6ba58ff6c1bc9ae077a855df2
SaarHV <dev16102@linx-info.com>
2022-12-14 20:35:00 +0800

    Add pagination component from element-plus

---
+++ src/components/common/pagination/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="block">
+    <span class="demonstration">调整每页显示条数</span>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      v-model:current-page="currentPage2"
+      :page-sizes="[100, 200, 300, 400]"
+      :page-size="100"
+      layout="sizes, prev, pager, next"
+      :total="1000"
+    />
+  </div>
+</template>
+<script lang="ts">
+export default {
+  methods: {
+    handleSizeChange(val: number) {
+      console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange(val: number) {
+      console.log(`当前页: ${val}`)
+    }
+  },
+  data() {
+    return {
+      currentPage1: 5,
+      currentPage2: 5,
+      currentPage3: 5,
+      currentPage4: 4
+    }
+  }
+}
+</script>
--- src/views/detection/basicService/index.vue
+++ src/views/detection/basicService/index.vue
@@ -1,8 +1,10 @@
 <template>
   basicService
   <Table />
+  <Pagination />
 </template>
 <script lang="ts" setup>
 import Table from '/@/components/common/table/index.vue'
+import Pagination from '/@/components/common/pagination/index.vue'
 </script>
 <style scoped lang="scss"></style>
