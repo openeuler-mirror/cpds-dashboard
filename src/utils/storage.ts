f315c73dca029a8f01e556ee190876143c8ba875
SaarHV <dev16102@linx-info.com>
2022-12-22 20:38:10 +0800

    Update storage's comment

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,58 +1,45 @@
 import Cookies from 'js-cookie'
 
-/**
- * window.localStorage
- * @method set
- * @method get
- * @method remove
- * @method clear
- */
+// window.localStorage
 export const Local = {
-  // set localStorage
+  // set
   set(key: string, val: any) {
     window.localStorage.setItem(key, JSON.stringify(val))
   },
-  // get localStorage by key
+  // get
   get(key: string) {
     const json: any = window.localStorage.getItem(key)
     return JSON.parse(json)
   },
-  // remove localStorage by key
+  // remove
   remove(key: string) {
     window.localStorage.removeItem(key)
   },
-  // remove all localStorage
+  // remove all
   clear() {
     window.localStorage.clear()
   }
 }
 
-/**
- * window.sessionStorage
- * if session key is token,we should oprate in cookie
- * @method set
- * @method get
- * @method remove
- * @method clear
- */
+// window.sessionStorage
 export const Session = {
-  // set sessionStorage
+  // set
   set(key: string, val: any) {
     if (key === 'token') return Cookies.set(key, val)
     window.sessionStorage.setItem(key, JSON.stringify(val))
   },
-  // get sessionStorage by key
+  // get
   get(key: string) {
     if (key === 'token') return Cookies.get(key)
     const json: any = window.sessionStorage.getItem(key)
     return JSON.parse(json)
   },
-  // remove sessionStorage by key
+  // remove
   remove(key: string) {
     if (key === 'token') return Cookies.remove(key)
     window.sessionStorage.removeItem(key)
   },
-  // clear all sessionStorage
+  // remove all
   clear() {
     Cookies.remove('token')
     window.sessionStorage.clear()
