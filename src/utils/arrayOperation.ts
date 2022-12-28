58bf3aa8e2218cfe6dcef6837177ed8a6246a483
SaarHV <dev16102@linx-info.com>
2022-12-28 18:23:18 +0800

    Add the function to judge whether two array strings are the same

--- src/utils/arrayOperation.ts
+++ src/utils/arrayOperation.ts
@@ -1,3 +1,17 @@
+// Judge whether two array strings are the same
+export function judementSameArr(newArr: unknown[] | string[], oldArr: string[]): boolean {
+  const news = removeDuplicate(newArr)
+  const olds = removeDuplicate(oldArr)
+  let count = 0
+  const leng = olds.length
+  for (const i in olds) {
+    for (const j in news) {
+      if (olds[i] === news[j]) count++
+    }
+  }
+  return count === leng
+}
+
 // Array and array object de duplication
 export function removeDuplicate(arr: any, attr?: string) {
   if (!arr && !arr.length) {
