16cef4638adfaeecdc392ecb99aae0c79ff83c9a
SaarHV <dev16102@linx-info.com>
2023-01-09 19:02:21 +0800

    Add multiple permission verifications

--- src/utils/authFunction.ts
+++ src/utils/authFunction.ts
@@ -4,3 +4,15 @@ export function auth(value: string): boolean {
   const stores = useUserStore()
   return stores.userInfos.authBtnList.some((v: string) => v === value)
 }
+
+// Multiple permission verifications, true if one is satisfied
+export function auths(value: Array<string>): boolean {
+  let flag = false
+  const stores = useUserStore()
+  stores.userInfos.authBtnList.map((val: string) => {
+    value.map((v: string) => {
+      if (val === v) flag = true
+    })
+  })
+  return flag
+}
