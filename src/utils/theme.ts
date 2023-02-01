9789a70b0b06b14620e8b997e29f246197b7d389
SaarHV <dev16102@linx-info.com>
2023-02-01 15:50:44 +0800

    Add function for changing rbg color to hex color

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -11,3 +11,12 @@ export function hexToRgb(str: any) {
   for (let i = 0; i < 3; i++) hexs[i] = parseInt(hexs[i], 16)
   return hexs
 }
+
+// Rgb color to Hex color
+export function rgbToHex(r: any, g: any, b: any) {
+  const reg = /^\d{1,3}$/
+  if (!reg.test(r) || !reg.test(g) || !reg.test(b)) return ElMessage.warning('输入错误的rgb颜色值')
+  const hexs = [r.toString(16), g.toString(16), b.toString(16)]
+  for (let i = 0; i < 3; i++) if (hexs[i].length == 1) hexs[i] = `0${hexs[i]}`
+  return `#${hexs.join('')}`
+}
