9e61594ec03665800c42ae47983a44aad7dd7900
SaarHV <dev16102@linx-info.com>
2023-02-01 09:14:03 +0800

    Add theme function:hex to rgb

---
+++ src/utils/theme.ts
@@ -0,0 +1,12 @@
+import { ElMessage } from 'element-plus'
+
+// Hex color to rgb color
+export function hexToRgb(str: any) {
+  let hexs: any = ''
+  const reg = /^\#?[0-9A-Fa-f]{6}$/
+  if (!reg.test(str)) return ElMessage.warning('输入错误的hex')
+  str = str.replace('#', '')
+  hexs = str.match(/../g)
+  for (let i = 0; i < 3; i++) hexs[i] = parseInt(hexs[i], 16)
+  return hexs
+}
