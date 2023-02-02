016e9b3f5899b41cc1cf3cb93acf46e239fe336f
SaarHV <dev16102@linx-info.com>
2023-02-02 10:42:26 +0800

    declare window variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,3 +6,7 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+// Declaration file, defining global variables
+interface Window {
+  nextLoading: boolean
+}
