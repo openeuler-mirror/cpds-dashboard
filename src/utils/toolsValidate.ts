c9e38b2aeabb22ff6eb66441ab15ac0b6ef543b8
SaarHV <dev16102@linx-info.com>
2023-02-02 09:33:19 +0800

    Add the function to generate uuid

--- src/utils/toolsValidate.ts
+++ src/utils/toolsValidate.ts
@@ -56,3 +56,12 @@ export function verifyEmail(val: string) {
     return false
   else return true
 }
+
+// Generate uuid
+export function guid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0
+    const v = c == 'x' ? r : (r & 0x3) | 0x8
+    return v.toString(16)
+  })
+}
