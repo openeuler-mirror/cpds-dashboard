4af3e877fb708c1608ba1509a9187bc29ebed37f
SaarHV <dev16102@linx-info.com>
2023-06-08 13:52:44 +0800

    Deleted unnecessary files and modified formatting

--- src/router/routeTitle.ts
+++ src/router/routeTitle.ts
@@ -2,7 +2,6 @@ export const nameToTitle: Record<string, any> = {
 	monitorWarn: { title: '监控警告', icon: 'jiankong' },
 	clusterStatus: { title: '集群状态', icon: 'jiqun' },
 	nodeHealth: { title: '节点健康', icon: 'jiedian' },
-	awrnManagement: { title: '警告管理', icon: 'jichuguanli' },
 	healthDiagnosis: { title: '健康诊断', icon: 'jiankangzhenduan' },
 	diagnosticResults: { title: '诊断结果', icon: 'jieguo' },
 	dataRetrieval: { title: '原始数据检索', icon: 'jieguojiancha' },
