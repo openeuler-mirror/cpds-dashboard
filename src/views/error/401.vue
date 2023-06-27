be8c1fc173469399006d6ba93b3ca8dd882469d8
SaarHV <dev16102@linx-info.com>
2023-06-27 11:08:37 +0800

    Add comments to the 401 page code

--- src/views/error/401.vue
+++ src/views/error/401.vue
@@ -12,7 +12,7 @@
 				</div>
 			</div>
 			<div class="right">
-				<img :src="noPermissionImg"/>
+				<img :src="noPermissionImg" />
 			</div>
 		</div>
 	</div>
@@ -32,10 +32,12 @@ const storesTagsViewRoutes = useTagsViewRoutes();
 const { themeConfig } = storeToRefs(storesThemeConfig);
 const { isTagsViewCurrenFull } = storeToRefs(storesTagsViewRoutes);
 const router = useRouter();
+//Reauthorization
 const onSetAuth = () => {
 	Session.clear();
 	router.push('/login');
 };
+//Initialize the height of the tag
 const initTagViewHeight = computed(() => {
 	let { isTagsview } = themeConfig.value;
 	if (isTagsViewCurrenFull.value) {
@@ -52,16 +54,19 @@ const initTagViewHeight = computed(() => {
 	height: 100%;
 	background-color: var(--el-color-white);
 	display: flex;
+
 	.error-flex {
 		margin: auto;
 		display: flex;
 		height: 350px;
 		width: 900px;
+
 		.left {
 			flex: 1;
 			height: 100%;
 			align-items: center;
 			display: flex;
+
 			.left-item {
 				.left-item-animation {
 					opacity: 0;
@@ -69,38 +74,43 @@ const initTagViewHeight = computed(() => {
 					animation-duration: 0.5s;
 					animation-fill-mode: forwards;
 				}
+
 				.left-item-num {
 					color: var(--el-color-info);
 					font-size: 55px;
 				}
+
 				.left-item-title {
 					font-size: 20px;
 					color: var(--el-text-color-primary);
 					margin: 15px 0 5px 0;
 					animation-delay: 0.1s;
 				}
+
 				.left-item-msg {
 					color: var(--el-text-color-secondary);
 					font-size: 12px;
 					margin-bottom: 30px;
 					animation-delay: 0.2s;
 				}
+
 				.left-item-btn {
 					animation-delay: 0.2s;
 				}
 			}
 		}
+
 		.right {
 			flex: 1;
 			opacity: 0;
 			animation-name: error-img;
 			animation-duration: 2s;
 			animation-fill-mode: forwards;
+
 			img {
 				width: 100%;
 				height: 100%;
 			}
 		}
 	}
-}
-</style>
+}</style>
