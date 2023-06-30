b8648328ae5e4c63d63a73ad9608aa93f5975fb8
SaarHV <dev16102@linx-info.com>
2023-06-30 15:10:55 +0800

    Dynamize units

--- src/components/usage/index.vue
+++ src/components/usage/index.vue
@@ -8,7 +8,7 @@
             <table>
                 <tbody>
                     <tr>
-                        <td>{{ props.name }} {{ props.unit }}</td>
+                        <td>{{ props.name }} {{ unit }}</td>
                     </tr>
                     <tr>
                         <td>{{ used }}</td>
@@ -23,7 +23,6 @@
 <script lang="ts" setup>
 import { ref, defineAsyncComponent, computed } from 'vue';
 const props = defineProps<{
-    unit: string
     name: string
     data: any
 }>()
@@ -33,10 +32,19 @@ const usageNum = computed(() => {
     return (props.data?.usage * 100)
 
 })
+const unit = computed(() => {
+    if (props.name === 'CPU') {
+        return 'cores'
+    } else {
+        if (props.data?.used_bytes > 1024 * 1024 * 1024 * 1024) return 'TB'
+        return 'GB'
+    }
+})
 const used = computed(() => {
     if (props.name === 'CPU') {
         return props.data?.used_core.toFixed(1) + '/' + props.data?.total_core?.toFixed(1);
     } else {
+        if (props.data?.used_bytes > 1024 * 1024 * 1024 * 1024) return (props.data?.used_bytes / Math.pow(1024, 4)).toFixed(1) + '/' + (props.data?.total_bytes / Math.pow(1024, 4)).toFixed(1)
         return (props.data?.used_bytes / Math.pow(1024, 3)).toFixed(1) + '/' + (props.data?.total_bytes / Math.pow(1024, 3)).toFixed(1)
     }
 
